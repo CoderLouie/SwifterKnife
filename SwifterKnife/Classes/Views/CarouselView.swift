3cfb9f428f286c2ab7f97c6007f51fbdd05bcff5
liyang <18616562401@163.com>
2021-10-26 19:40:58 +0800

    1. CarouselView权限问题

--- SwifterKnife/Classes/Views/CarouselView.swift
+++ SwifterKnife/Classes/Views/CarouselView.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // MARK: - CarouselViewDelegate
-protocol CarouselViewDelegate: AnyObject {
+public protocol CarouselViewDelegate: AnyObject {
     func carouselView(_ carouselView: CarouselView, didSelect cell: CarouselViewCell, at index: Int)
     func carouselView(_ carouselView: CarouselView, didDeselect cell: CarouselViewCell, at index: Int)
     func carouselView(_ carouselView: CarouselView, willAppear cell: CarouselViewCell, at index: Int)
@@ -25,25 +25,25 @@ extension CarouselViewDelegate {
 
 // MARK: - CarouselViewCell
 
-class CarouselViewCell: UIView {
-    override init(frame: CGRect) {
+open class CarouselViewCell: UIView {
+    public override init(frame: CGRect) {
         super.init(frame: .zero)
         setup()
     }
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    func setup() { }
+    open func setup() { }
 }
 
 
 // MARK: - CarouselView
 /// 无限循环滚动控件
-class CarouselView: UIView {
+open class CarouselView: UIView {
     
-    public weak var delegate: CarouselViewDelegate?
+    open weak var delegate: CarouselViewDelegate?
     
-    private lazy var scrollView = UIScrollView().then {
+    public private(set) lazy var scrollView = UIScrollView().then {
         $0.showsVerticalScrollIndicator = false
         $0.showsHorizontalScrollIndicator = false
         $0.isPagingEnabled = true
@@ -63,7 +63,7 @@ class CarouselView: UIView {
     private var nextIndex = -1
     private unowned var nextCell: CarouselViewCell!
     /// 数据源数量
-    var itemsCount: Int = 0 {
+    open var itemsCount: Int = 0 {
         willSet {
             guard itemsCount == 0 else {
                 fatalError("this property only can modify once")
@@ -112,7 +112,7 @@ class CarouselView: UIView {
         }
     }
     
-    override func layoutSubviews() {
+    open override func layoutSubviews() {
         super.layoutSubviews()
         
         guard scrollView.bounds.isEmpty else {
@@ -137,17 +137,17 @@ class CarouselView: UIView {
 
 // MARK: - Public Method
 extension CarouselView {
-    var isTracking: Bool { scrollView.isTracking }
-    var isDragging: Bool { scrollView.isDragging }
-    var isDecelerating: Bool { scrollView.isDecelerating }
+    open var isTracking: Bool { scrollView.isTracking }
+    open var isDragging: Bool { scrollView.isDragging }
+    open var isDecelerating: Bool { scrollView.isDecelerating }
     
-    public var isScrollEnabled: Bool {
+    open var isScrollEnabled: Bool {
         get { scrollView.isScrollEnabled }
         set { scrollView.isScrollEnabled = newValue }
     }
     
     /// 滚动到指定位置
-    func scrollToIndex(_ index: Int) {
+    open func scrollToIndex(_ index: Int) {
         guard index >= 0 else { return }
         let idx = index % itemsCount
         guard currentIndex != idx else { return }
@@ -159,15 +159,15 @@ extension CarouselView {
         }
     }
     
-    func forward() {
+    open func forward() {
         scrollView.setContentOffset(CGPoint(x: side * 2, y: 0), animated: true)
     }
-    func backward() { 
+    open func backward() {
         scrollView.setContentOffset(.zero, animated: true)
     }
     
     /// 注册cell
-    func register<T: CarouselViewCell>(_ cellClass: T.Type) {
+    open func register<T: CarouselViewCell>(_ cellClass: T.Type) {
         currentCell = T().then {
             $0.clipsToBounds = true
             scrollView.addSubview($0)
@@ -181,7 +181,7 @@ extension CarouselView {
 
 // MARK: - UIScrollViewDelegate
 extension CarouselView: UIScrollViewDelegate {
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         scrollingDidEnd()
     }
     private func scrollingDidEnd() {
@@ -195,13 +195,12 @@ extension CarouselView: UIScrollViewDelegate {
         reset()
     }
     
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard scrollView.contentSize != .zero else {
             return
         }
         let offsetX = scrollView.contentOffset.x
-        
-//        Console.log("CarouselView \(offsetX)")
+         
         if offsetX > side {
             direction = .left
             if offsetX > side * 2.05 {
@@ -216,7 +215,7 @@ extension CarouselView: UIScrollViewDelegate {
             direction = .none
         }
     }
-    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+    public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
         scrollingDidEnd()
     }
      
