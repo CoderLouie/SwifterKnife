e74d6cca485c0b398a6e4b471938fa9bb2b5dea1
liyang <18616562401@163.com>
2021-11-23 14:53:08 +0800

    1. xxx

--- SwifterKnife/Classes/Extension/QuartzCore/CALayer+Add.swift
+++ SwifterKnife/Classes/Extension/QuartzCore/CALayer+Add.swift
@@ -7,14 +7,30 @@
 
 import QuartzCore
 
+// MARK: - Properties
+
 public extension CALayer {
+    /// 核心动画是否处于暂停状态
+    var animationIsPaused: Bool { timeOffset > 0 }
+}
+
+// MARK: - Methods
+
+public extension CALayer {
+    
+    /// 暂停核心动画
     func pauseAnimation() {
+        let now = convertTime(CACurrentMediaTime(), from: nil)
         speed = 0
-        timeOffset = convertTime(CACurrentMediaTime(), from: nil)
+        timeOffset = now
     }
+    
+    /// 继续执行核心动画
     func resumeAnimation() {
+        let pauseTime = timeOffset
         speed = 1.0
-        beginTime = convertTime(CACurrentMediaTime(), from: nil) - timeOffset
         timeOffset = 0
+        beginTime = 0
+        beginTime = convertTime(CACurrentMediaTime(), from: nil) - pauseTime
     }
 }
