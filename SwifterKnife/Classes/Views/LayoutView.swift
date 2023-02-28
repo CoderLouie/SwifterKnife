749d50371cde207dd50431b6d8b36e48c4c5aa24
liyang <18616562401@163.com>
2023-02-28 17:40:36 +0800

    1. xxx

--- SwifterKnife/Classes/Views/LayoutView.swift
+++ SwifterKnife/Classes/Views/LayoutView.swift
@@ -45,4 +45,97 @@ open class SpaceView: UIView {
         return intrinsicSize
     }
 }
- 
+
+
+public final class SudokuView: VirtualView {
+    public var columnCount = 3
+    public var rowCount: Int? = nil
+    public var marginX: CGFloat = 8
+    public var marginY: CGFloat = 8
+    
+    public enum Alignment {
+        case top, center, bottom
+    }
+    public var cellAlignment: Alignment = .center
+    
+    private var insHeight = UIView.noIntrinsicMetric
+    
+    public override var intrinsicContentSize: CGSize {
+        var size = super.intrinsicContentSize
+        if rowCount == nil {
+            size.height = insHeight
+        }
+        return size
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        guard columnCount > 0, !subviews.isEmpty else { return }
+        
+        let bounds = bounds
+        let width = bounds.width
+        let height = bounds.height
+        guard width > 0 else { return }
+        
+        let colN = CGFloat(columnCount)
+        let w = ((width - (colN - 1) * marginX) / colN).pixFloor
+        var x: CGFloat = 0, y: CGFloat = 0
+        if let n = rowCount {
+            let rowN = CGFloat(n)
+            guard height > 0 else { return }
+            let h = ((height - (rowN - 1) * marginY) / rowN).pixFloor
+            for (i, subview) in subviews.enumerated() {
+                if i != 0, i % columnCount == 0 {
+                    x = 0; y += marginY + h
+                }
+                subview.frame = CGRect(x: x, y: y, width: w, height: h)
+                x += w + marginX
+            }
+        } else {
+            var rowViews: [UIView] = []
+            var rowMaxH: CGFloat = 0
+            var rowHeights: [CGFloat] = []
+            for (i, subview) in subviews.enumerated() {
+                if i != 0, i % columnCount == 0 {
+                    for (j, cell) in rowViews.enumerated() {
+                        let h = rowHeights[j]
+                        let space: CGFloat
+                        switch cellAlignment {
+                        case .top: space = 0
+                        case .center: space = (rowMaxH - h) * 0.5
+                        case .bottom: space = rowMaxH - h
+                        }
+                        cell.frame = CGRect(x: x, y: y + space, width: w, height: h)
+                        x += w + marginX
+                    }
+                    x = 0
+                    y += rowMaxH + marginY
+                    rowHeights.removeAll()
+                    rowViews.removeAll()
+                    rowMaxH = 0
+                }
+                rowViews.append(subview)
+                let subVH = subview.frame.height
+                let h = subVH > 0 ? subVH : subview.systemLayoutSizeFitting(
+                    CGSize(width: w, height: 0),
+                    withHorizontalFittingPriority: .required,
+                    verticalFittingPriority: .fittingSizeLevel).height.pixCeil
+                rowMaxH = max(rowMaxH, h)
+                rowHeights.append(h)
+            }
+            for (j, cell) in rowViews.enumerated() {
+                let h = rowHeights[j]
+                let space: CGFloat
+                switch cellAlignment {
+                case .top: space = 0
+                case .center: space = (rowMaxH - h) * 0.5
+                case .bottom: space = rowMaxH - h
+                }
+                cell.frame = CGRect(x: x, y: y + space, width: w, height: h)
+                x += w + marginX
+            }
+            insHeight = y + rowMaxH
+            invalidateIntrinsicContentSize()
+        }
+    }
+}
