358e8b77eedbe36053ea43cec32a650d2ef541bb
liyang <18616562401@163.com>
2025-08-23 15:19:02 +0800

    1. xxx

--- SwifterKnife/Classes/Views/ScreenUtils/SKWindow.swift
+++ SwifterKnife/Classes/Views/ScreenUtils/SKWindow.swift
@@ -233,7 +233,7 @@ public enum SKS {
             DispatchQueue.main.async(execute: ops)
         }
     }
-    public static func makeScreenOptions(_ make: (_ maker: SKSOptionHandler) -> Void) {
+    public static func makeScreenOptions(_ make: @escaping (_ maker: SKSOptionHandler) -> Void) {
         let ops = {
             guard let opvc = rootVC(at: 1) as? OptionsViewController else { return }
             make(opvc)
